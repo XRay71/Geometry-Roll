ec7a2d359548f767be426640831f77a9dbabd767
Ray Hang <ray200555@gmail.com>
2022-06-13 13:37:00 -0400

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -1,5 +1,10 @@
 public class Constants {
 	
-	public static final String BACKGROUND_IMAGE = "";
-
+	public static String BACKGROUND_IMAGE = "";
+	public static int GAME_HEIGHT;
+	public static int GAME_WIDTH;
+	public static int GRID_UNIT;
+	public static int GAME_SPEED;
+	public static int GRAVITY;
+	
 }
