736d8ec2ed5f0242937ae78d25ac78d6ce46cc11
Ray Hang <ray200555@gmail.com>
2022-06-13 22:43:56 -0400

    Add files via upload

--- Assets/Classes/Constants.java
+++ Assets/Classes/Constants.java
@@ -1,12 +1,27 @@
 package Assets.Classes;
 
 public class Constants {
-	
-	public static String BACKGROUND_IMAGE = "";
-	public static int GAME_HEIGHT;
-	public static int GAME_WIDTH;
-	public static int GRID_UNIT;
-	public static int GAME_SPEED;
-	public static int GRAVITY;
-	
+
+	// game constants
+	public static final int GAME_HEIGHT = 540;
+	public static final int GAME_WIDTH = 1280;
+	public static final int GRID_UNIT = 10;
+	public static final double GRAVITY = -0.075;
+
+	// background images
+	public static final String LVL1_BACKGROUND = "";
+	public static final String LVL2_BACKGROUND = "";
+
+	// game speeds
+	public static int GAME_SPEED = 5;
+	public static final int LVL1_SPEED = 5;
+	public static final int LVL2_SPEED = 10;
+	public static final int LVL3_SPEED = 16;
+
+	// player sprites
+	public static final String PLAYER_MODE_ZERO_SPRITE = "";
+	public static final String PLAYER_MODE_ONE_SPRITE = "";
+	public static final String PLAYER_MODE_TWO_SPRITE = "";
+	public static final String PLAYER_MODE_THREE_SPRITE = "";
+
 }
